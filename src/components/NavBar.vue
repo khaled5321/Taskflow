d4964c5794de0edbc4e72d110652ea91f4d3bc9c
Khaled Emad <khaledemad656@gmail.com>
2023-02-02 22:28:37 +0200

    edited css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -18,23 +18,41 @@ const changeTheme = () => {
 </script>
 
 <template>
-    <nav class="container">
-        <h2>TASKFLOW</h2>
-        <ul>
-            <li>
-                <a href="#" role="button" @click="changeTheme()">Theme</a>
-            </li>
-            <li>
-                <RouterLink to="/" role="button" class="secondary">Login</RouterLink>
-            </li>
-        </ul>
-    </nav>
+    <header>
+        <nav class="container">
+            <h2>TASKFLOW</h2>
+            <ul>
+                <li>
+                    <a href="#" role="button" @click="changeTheme()">Theme</a>
+                </li>
+                <li>
+                    <RouterLink to="/" role="button" class="secondary">Login</RouterLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template> 
 
 <style scoped>
-h2{
-    margin:0px
+/* header */
+header{
+    background-color:#0e1318;
+    box-shadow: 0px 1px 1px #d2d3d3;
 }
+[data-theme="light"] header{
+    background-color: rgb(121, 121, 121);
+    box-shadow: 0px 1px 1px #000;
+}
+/* Title */
+[data-theme="light"] h2{
+    color: rgb(244, 248, 254);
+}
+h2{
+    margin:0px;
+    /* color: rgb(204, 94, 43); */
+    text-shadow: 2px 2px 2px #000;
+}
+/* navbar */
 .container{
     padding-top: 12px;
     align-items: center;
