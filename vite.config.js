62662d89962ca4a0da5e09c26f93bcf3707655ff
Khaled Emad <khaledemad656@gmail.com>
2023-02-11 18:54:09 +0200

    added PWA

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,35 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import htmlPurge from 'vite-plugin-purgecss';
+import viteCompression from 'vite-plugin-compression';
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteCompression(), htmlPurge([htmlPurge()]), VitePWA({ 
+    registerType: 'autoUpdate', 
+    includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'TaskFlow',
+        short_name: 'TaskFlow',
+        description: 'Todo application',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+    devOptions: {
+    enabled: true
+  } })], 
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
